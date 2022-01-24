e2b515ef43be6a8f47c8031a5050cad91c6bb378
marvincd <61430224+marvincd@users.noreply.github.com>
2022-01-24 22:50:56 +0000

    body view centered

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../widgets/navigation_bar/nav_bar.dart';
+import '../../widgets/centered_view/centered_view.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -7,10 +8,14 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
       return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(children: <Widget>[
-          NavBar()
-        ],),
+        backgroundColor: Colors.white,
+        body: CentredView(
+          child: Column(
+            children: <Widget>[
+              NavBar(),
+            ],
+          ),
+        ),
       );
   }
 }
