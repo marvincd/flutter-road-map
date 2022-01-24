50b2a7c574abe290f8ebe5d43404fa5d496c8e09
marvincd <61430224+marvincd@users.noreply.github.com>
2022-01-24 22:38:31 +0000

    navigation code modularization

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,46 +1,17 @@
 import 'package:flutter/material.dart';
+import '../../widgets/navigation_bar/nav_bar.dart';
 
 class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-   return Container(
-      height: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          SizedBox(
-            height: 80,
-            width: 150,
-            child: Image.asset('logo.png'),
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              _NavBarItem('Episodes'),
-              SizedBox(
-                width: 60,
-              ),
-              _NavBarItem('About'),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class _NavBarItem extends StatelessWidget {
-  final String title;
-  const _NavBarItem(
-    this.title, {
-    Key? key,
-  }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: TextStyle(fontSize: 18),
-    );
+      return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(children: <Widget>[
+          NavBar()
+        ],),
+      );
   }
 }
+
